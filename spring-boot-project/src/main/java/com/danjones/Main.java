7c3f9230905c67bca94305a906634772afa5b3cc
Daniel Jones <dajones@mgmresorts.com>
2023-02-16 03:42:08 -0500

    made first instance of CRUD in main, starting member CRUD, starting docker

---
+++ spring-boot-project/src/main/java/com/danjones/Main.java
@@ -0,0 +1,66 @@
+package com.danjones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public GreetResponse greet() {
+        return new GreetResponse(
+                "Hello",
+                List.of("MonstersInc", "Uncharted", "Chronicle"),
+                new Person("Daniel", 27, 65_000));
+    }
+
+    record Person(String name, int age, double salary) {
+
+    }
+
+    record GreetResponse(
+            String greet,
+            List<String> topMovies,
+            Person person){}
+
+//    class GreetResponse{
+//        private final String greet;
+//
+//
+//        GreetResponse(String greet) {
+//            this.greet = greet;
+//        }
+//
+//        public String getGreet() {
+//            return greet;
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return super.toString();
+//        }
+//
+//        @Override
+//        public boolean equals(Object o) {
+//            if (this == o) return true;
+//            if (o == null || getClass() != o.getClass()) return false;
+//            GreetResponse that = (GreetResponse) o;
+//            return Objects.equals(greet, that.greet);
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return Objects.hash(greet);
+//        }
+//    }
+
+}
---
+++ spring-boot-project/src/main/java/com/danjones/Member.java
@@ -0,0 +1,78 @@
+package com.danjones;
+
+import java.util.Objects;
+
+public class Member {
+    private Integer id;
+    private String name;
+    private String tier;
+    private Integer subscribedYears;
+
+    public Member(Integer id,
+                  String name,
+                  String tier,
+                  Integer subscribedYears) {
+        this.id = id;
+        this.name = name;
+        this.tier = tier;
+        this.subscribedYears = subscribedYears;
+    }
+
+    public Member() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTier() {
+        return tier;
+    }
+
+    public void setTier(String tier) {
+        this.tier = tier;
+    }
+
+    public Integer getSubscribedYears() {
+        return subscribedYears;
+    }
+
+    public void setSubscribedYears(Integer subscribedYears) {
+        this.subscribedYears = subscribedYears;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(id, member.id) && Objects.equals(name, member.name) && Objects.equals(tier, member.tier) && Objects.equals(subscribedYears, member.subscribedYears);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, tier, subscribedYears);
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", tier='" + tier + '\'' +
+                ", subscribedYears=" + subscribedYears +
+                '}';
+    }
+}
